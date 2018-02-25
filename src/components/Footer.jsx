a1ecf15d515955e55403ee7f221ea0b6a4025799
rewrewby <andrei.kuts@gmail.com>
2018-02-25 20:37:11 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,33 +18,13 @@ export default class Footer extends Component {
                     <Col xs={12} className="footerSocialWrap">
                         <ul className="footerSocial">
                             <li>
-                                <a href="http://zclassic.org/">
-                                    <img src={website} alt="website"/>
+                                <a href="https://btcprivate.org">
+                                    <img src={website} alt="website" title="Official site"/>
                                 </a>
                             </li>
                             <li>
-                                <a href="https://github.com/z-classic/zclassic">
-                                    <img src={github} alt="github"/>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://twitter.com/ZclassicCoin">
-                                    <img src={twitter} alt="twitter"/>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://discord.gg/PmVX5jW">
-                                    <img src={discord} alt="discord"/>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://bitcointalk.org/index.php?topic=1671982.msg16789011#msg16789011">
-                                    <img src={bitcointalk} alt="bitcointalk"/>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://www.reddit.com/r/ZClassic/">
-                                    <img src={reddit} alt="reddit"/>
+                                <a href="https://btcprivate.pro">
+                                    <img src={bitcointalk} alt="bitcointalk" title="Mining Pool"/>
                                 </a>
                             </li>
                         </ul>
@@ -53,14 +33,6 @@ export default class Footer extends Component {
 
                     </Col>
                 </Row>
-                </div>
-                <div>
-                <p className="footerCopyright">
-                    <a href="https://github.com/johandjoz/zclassicpaperwallet.com" target="_blank" rel="noopener noreferrer">JavaScript Client-Side ZCLASSIC Wallet Generator</a>
-                </p>
-                <p className="footerCopyright">
-                    Send me a memo @ zcH8yxWM5HHkrLKrt9HeTxWYPUqv1kzLYtX44BmkFSvWaixmfibB4U64P81RpDUUtGYuWRW1TPDb4uiB9nPtMfBrBcPv4w7
-                </p>
                 </div>
             </Grid>
         );
